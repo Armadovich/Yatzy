062ba8464243cbcf7e4a11e6bac716f6c6f517b0
Armadovich+ <armada.dag@gmail.com>
2023-12-15 02:49:42 +0100

    refactor(Yatzy,YatzyTest)refactoriza three_of_a_kind

--- src/main/java/edu/gorilas/Yatzy.java
+++ src/main/java/edu/gorilas/Yatzy.java
@@ -108,6 +108,19 @@ public class Yatzy {
         return score;
     }
 
+    public static int three_of_a_kind(int[] nums) {
+        int contador = 0;
+        for (int i = 6; i > 0; i--) {
+            for (int num : nums) {
+                if (num == i) contador++;
+                if (contador == 3) return i * 3;
+            }
+            contador = 0;
+        }
+        return NULL_SCORE;
+
+    }
+
     public static int four_of_a_kind(int _1, int _2, int d3, int d4, int d5)
         {
             int[] tallies;
@@ -123,20 +136,7 @@ public class Yatzy {
             return 0;
         }
 
-    public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5)
-        {
-            int[] t;
-            t = new int[6];
-            t[d1-1]++;
-            t[d2-1]++;
-            t[d3-1]++;
-            t[d4-1]++;
-            t[d5-1]++;
-            for (int i = 0; i < 6; i++)
-                if (t[i] >= 3)
-                    return (i+1) * 3;
-            return 0;
-        }
+
 
     public static int smallStraight(int d1, int d2, int d3, int d4, int d5)
         {
--- src/test/java/edu/gorilas/YatzyTest.java
+++ src/test/java/edu/gorilas/YatzyTest.java
@@ -78,16 +78,16 @@ public class YatzyTest {
     @Test
     public void test_three_of_a_kind()
     {
-        assertEquals(9, Yatzy.three_of_a_kind(3,3,3,4,5));
-        assertEquals(15, Yatzy.three_of_a_kind(5,3,5,4,5));
-        assertEquals(9, Yatzy.three_of_a_kind(3,3,3,3,5));
+        assertEquals(9, Yatzy.three_of_a_kind(new int[]{3,3,3,4,5}));
+        assertEquals(15, Yatzy.three_of_a_kind(new int[]{5,3,5,4,5}));
+        assertEquals(9, Yatzy.three_of_a_kind(new int[]{3,3,3,3,5}));
     }
 
     @Test
     public void test_four_of_a_knd() {
         assertEquals(12, Yatzy.four_of_a_kind(3,3,3,3,5));
         assertEquals(20, Yatzy.four_of_a_kind(5,5,5,4,5));
-        assertEquals(9, Yatzy.three_of_a_kind(3,3,3,3,3));
+        assertEquals(9, Yatzy.three_of_a_kind(new int[]{3,3,3,3,3}));
     }
 
     @Test
